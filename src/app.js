4e5288f3131071d5b011b236080002b4bab8f86c
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2024-05-11 05:47:20 +0200

    use region enums, add getMatchDetails routing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 require('dotenv').config({ path: '../.env' });
-const { getPlayerPuuid } = require('./services/riotApiService');
+const { getPlayerPuuid, getMatchHistory, getMatchDetails } = require('./services/riotApiService');
 const playerRoutes = require('./routes/playerRoutes');
-
+const { Region } = require('./enums/region');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -18,9 +18,18 @@ app.listen(PORT, async () => {
     
     try {
         // Call the getPlayerPuuid function from the riotApiService
-        const puuid = await getPlayerPuuid('PewPewPotato', '123');
+        const puuid = await getPlayerPuuid(Region.EUROPE,'PewPewPotato', '123');
+
         console.log('PUUID:', puuid);
+        // Call the getMatchHistory function from the riotApiService
+        const matchHistory = await getMatchHistory(Region.EUROPE, puuid);
+        console.log('Match History:', matchHistory);
+
+        // Call the getMatchDetails function for first match in list from the riotApiService
+        const matchDetails = await getMatchDetails(Region.EUROPE, matchHistory[0]);
+        console.log('Match Details:', matchDetails);
+
     } catch (error) {
-        console.error('Error fetching PUUID:', error);
+        console.error('Error fetching Match Details:', error);
     }
 });
