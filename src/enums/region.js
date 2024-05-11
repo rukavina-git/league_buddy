1af86316ad005a490bc37ebe24febc4c4deb8a16
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2024-05-11 05:46:16 +0200

    Add enums

---
+++ src/enums/region.js
@@ -0,0 +1,8 @@
+const Region = {
+    AMERICAS: 'americas.api.riotgames.com',
+    ASIA: 'asia.api.riotgames.com',
+    EUROPE: 'europe.api.riotgames.com',
+    SEA: 'sea.api.riotgames.com',
+};
+
+module.exports = { Region };
